62ece51f315f59bc62fd10ce2bdeb313db4f1458
whosAle <abelgrave95@gmail.com>
2019-04-16 11:55:51 -0400

    add destroy feature and made buttons in erbs

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,5 @@
 class ReviewsController < ApplicationController
-  before_action :find_review, only: [:edit, :destroy]
+  before_action :find_review, only: [:edit]
 
   def new
     @review = Review.new
@@ -8,7 +8,6 @@ class ReviewsController < ApplicationController
   end
 
   def create
-    
     @review = Review.create(review_params)
     if @review.valid?
       redirect_to activity_path(@review.activity)
@@ -27,7 +26,14 @@ class ReviewsController < ApplicationController
   end
 
   def destroy
-
+    byebug
+    @review = Review.find(params[:review_id])
+    @review.destroy
+    if params[:user_show]
+      redirect_to user_path(@review.user)
+    else
+      redirect_to activity_path(@review.activity)
+    end
   end
 
   private
