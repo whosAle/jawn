5bb85889ef18ff85998f39146e5ee98503325f6c
whosAle <abelgrave95@gmail.com>
2019-04-16 17:26:57 -0400

    remove comments

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,13 +7,12 @@ class SessionsController < ApplicationController
   def create
     #TODO change back to user_name
     user = User.find_by(name: params[:session][:user_name])
-   #TODO authenticate password in the if
+
     if user && user.authenticate(params[:session][:password])
       log_in user
       redirect_to user
     else
-#      flash[:danger] = user.errors.full_messages
-      flash.now[:danger] = "Invalid name"
+      flash.now[:danger] = "Invalid user and password combination"
       render "new"
     end
   end
