6ae434031d3801360cb9ff743df701e8bfd96ac7
joshuamsmith413 <joshuamsmith413@gmail.com>
2019-04-17 11:12:06 -0400

    created a friendship model, table, and relationship

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_16_204521) do
+ActiveRecord::Schema.define(version: 2019_04_17_150013) do
 
   create_table "activities", force: :cascade do |t|
     t.string "name"
@@ -20,6 +20,13 @@ ActiveRecord::Schema.define(version: 2019_04_16_204521) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "url_link"
+  end
+
+  create_table "friendships", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "followee_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "neighborhoods", force: :cascade do |t|
