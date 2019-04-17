c053695c452af27bdd44d95054bda33642afae2b
joshuamsmith413 <joshuamsmith413@gmail.com>
2019-04-17 16:25:58 -0400

    added a navbar

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,5 @@ Rails.application.routes.draw do
 
   get '/login', to: 'sessions#new', as: 'login'
   post '/login', to: 'sessions#create'
-  get '/logout', to: 'sessions#destroy', as: 'logout'
+  delete '/logout', to: 'sessions#destroy', as: 'logout'
 end
